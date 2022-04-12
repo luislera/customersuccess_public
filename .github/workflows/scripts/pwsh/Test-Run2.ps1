1821ba3173a5c9985ece44a7efdc0c9ac7ad98fc
Mica <97895605+mraitersw@users.noreply.github.com>
2022-04-12 13:46:22 -0300

    Update Test-Run2.ps1

--- .github/workflows/scripts/pwsh/Test-Run2.ps1
+++ .github/workflows/scripts/pwsh/Test-Run2.ps1
@@ -1,3 +1,3 @@
 function Run-Workflow ($githubRef, $solution_name, $environment_url, $source_branch, $branch_to_create, $commit_message) {
-    gh workflow run determine-solution-build-deploy --ref 
+    gh workflow run determine-solution-build-deploy
 }

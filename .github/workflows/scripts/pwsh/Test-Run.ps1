b545f4cd20e3c2c3e90c0c4b631308c67b7c73a8
matolera <matiaslera@gmail.com>
2022-04-12 12:11:07 -0300

    pshell udpate

--- .github/workflows/scripts/pwsh/Test-Run.ps1
+++ .github/workflows/scripts/pwsh/Test-Run.ps1
@@ -2,5 +2,5 @@ function Run-Workflow ($githubRef, $solution_name, $environment_url, $source_bra
     gh workflow run export-unpack-commit-solution --ref $githubRef -f solution_name=$solution_name -f environment_url=$environment_url -f source_branch=$source_branch -f branch_to_create=$branch_to_create -f commit_message=$commit_message
 
     echo "pipeline queued for $solution_name"
-    echo "pipeline queued for $environment_url"
+    gh workflow view export-unpack-commit-solution
 }

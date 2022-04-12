1b3a07cea2991681e6b0d09fa39ba63300626179
Mica <97895605+mraitersw@users.noreply.github.com>
2022-04-12 11:44:20 -0300

    Update Test-Run2.ps1

--- .github/workflows/scripts/pwsh/Test-Run2.ps1
+++ .github/workflows/scripts/pwsh/Test-Run2.ps1
@@ -1,6 +1,3 @@
 function Run-Workflow ($githubRef, $solution_name, $environment_url, $source_branch, $branch_to_create, $commit_message) {
-    gh workflow run determine-solution-build-deploy --ref $githubRef -f solution_name=$solution_name -f environment_url=$environment_url -f source_branch=$source_branch -f branch_to_create=$branch_to_create -f commit_message=$commit_message
-
-    echo "pipeline queued for $solution_name"
-    echo "pipeline queued for $environment_url"
+    gh workflow run determine-solution-build-deploy --ref 
 }

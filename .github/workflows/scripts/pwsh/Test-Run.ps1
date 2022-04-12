a5439215363e8cd70ae4aa87e30da3aba0476a1f
matolera <matiaslera@gmail.com>
2022-04-12 12:20:37 -0300

    Update Test-Run.ps1

--- .github/workflows/scripts/pwsh/Test-Run.ps1
+++ .github/workflows/scripts/pwsh/Test-Run.ps1
@@ -3,6 +3,7 @@ function Run-Workflow ($githubRef, $solution_name, $environment_url, $source_bra
     gh workflow run export-unpack-commit-solution --ref $githubRef -f solution_name=$solution_name -f environment_url=$environment_url -f source_branch=$source_branch -f branch_to_create=$branch_to_create -f commit_message=$commit_message
     echo "sleep for 5 seconds"
     Start-Sleep -Seconds 5
-    gh workflow view export-unpack-commit-solution
+    $cmdOutput = gh workflow view export-unpack-commit-solution
+    echo "cmdOutput : $cmdOutput"
     echo "end Run-Workflow"
 }

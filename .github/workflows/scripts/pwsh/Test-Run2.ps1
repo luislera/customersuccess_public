049ac196345c186697758b3941fc1105a2aa55d1
Mica <97895605+mraitersw@users.noreply.github.com>
2022-04-12 14:43:47 -0300

    Update Test-Run2.ps1

--- .github/workflows/scripts/pwsh/Test-Run2.ps1
+++ .github/workflows/scripts/pwsh/Test-Run2.ps1
@@ -1,4 +1,9 @@
-function Run-Workflow () {
+function Run-Workflow ($githubRef, $solution_name) {
     echo "init Run-Workflow"
-    gh workflow run determine-solution-build-deploy
+    gh workflow run export-unpack-commit-solution --ref $githubRef -f solution_name=$solution_name -f
+    echo "sleep for 5 seconds"
+    Start-Sleep -Seconds 5
+    $cmdOutput = gh workflow view export-unpack-commit-solution | Out-String
+    echo "cmdOutput : $cmdOutput" 
+    echo "end Run-Workflow"
 }

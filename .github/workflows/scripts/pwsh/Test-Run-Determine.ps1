e6f40220cccf1760dde5f1a23f3320f1960ef7d9
Federico Bernal <64086728+FedericoBernal@users.noreply.github.com>
2022-04-12 15:03:03 -0300

    Update Test-Run-Determine.ps1

--- .github/workflows/scripts/pwsh/Test-Run-Determine.ps1
+++ .github/workflows/scripts/pwsh/Test-Run-Determine.ps1
@@ -1,6 +1,6 @@
 function Run-Workflow ($githubRef, $solution_name) {
     echo "init Run-Workflow"
-    gh workflow run determine-solution-build-deploy --ref $githubRef -f solution_name=$solution_name -f
+    gh workflow run determine-solution-build-deploy --ref $githubRef -f solution_name=$solution_name
     echo "sleep for 5 seconds"
     Start-Sleep -Seconds 5
     $cmdOutput = gh workflow view determine-solution-build-deploy | Out-String

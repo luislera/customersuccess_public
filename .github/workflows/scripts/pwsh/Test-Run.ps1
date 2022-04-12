6244be5614d75943368a77f0abb4db915a267a4a
matolera <matiaslera@gmail.com>
2022-04-12 15:24:34 -0300

    Update Test-Run.ps1

--- .github/workflows/scripts/pwsh/Test-Run.ps1
+++ .github/workflows/scripts/pwsh/Test-Run.ps1
@@ -1,11 +1,16 @@
 function Run-Workflow ($githubRef, $solution_name, $environment_url, $source_branch, $branch_to_create, $commit_message) {
     echo "init Run-Workflow"
-    #gh workflow run export-unpack-commit-solution --ref $githubRef -f solution_name=$solution_name -f environment_url=$environment_url -f source_branch=$source_branch -f branch_to_create=$branch_to_create -f commit_message=$commit_message
-    echo "sleep for 5 seconds"
-    #Start-Sleep -Seconds 5
-    $cmdOutput = gh workflow view export-unpack-commit-solution | Out-String
-    $line = ($cmdOutput -split '\n')[5]
-    $status = ($line -split '	')[0]
-    echo "workflow status : $status" 
+    gh workflow run export-unpack-commit-solution --ref $githubRef -f solution_name=$solution_name -f environment_url=$environment_url -f source_branch=$source_branch -f branch_to_create=$branch_to_create -f commit_message=$commit_message
+
+    Do
+    {
+        echo "sleep for 5 seconds"
+        Start-Sleep -Seconds 5
+        $cmdOutput = gh workflow view export-unpack-commit-solution | Out-String
+        $firstLine = ($cmdOutput -split '\n')[5]
+        $status = ($firstLine -split '	')[0]
+        echo "workflow status : $status" 
+    } Until ($status -ne "completed") 
+    
     echo "end Run-Workflow"
 }

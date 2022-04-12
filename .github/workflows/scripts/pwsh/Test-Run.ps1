5c5f542ce23a4c700bf2f71dcc0b6d1437d798e8
matolera <matiaslera@gmail.com>
2022-04-12 16:08:49 -0300

    update

--- .github/workflows/scripts/pwsh/Test-Run.ps1
+++ .github/workflows/scripts/pwsh/Test-Run.ps1
@@ -1,21 +1,18 @@
 function Export-Unpack-Commit ($githubRef, $workflow_name, $solution_name, $environment_url, $source_branch, $branch_to_create, $commit_message) {
     gh workflow run $workflow_name --ref $githubRef -f solution_name=$solution_name -f environment_url=$environment_url -f source_branch=$source_branch -f branch_to_create=$branch_to_create -f commit_message=$commit_message
 
-    Do
-    {
-        Start-Sleep -Seconds 10
-        $cmdOutput = gh workflow view $workflow_name | Out-String
-        $firstLine = ($cmdOutput -split '\n')[5]
-        $status = ($firstLine -split '	')[0]
-        echo "$workflow_name status: $status" 
-    } While ($status -ne "completed")
+    Wait-Workflow-Execution $workflow_name
 }
 function Sync-Unmanaged ($githubRef, $workflow_name, $from_branch, $to_branch) {
     gh workflow run $workflow_name --ref $githubRef -f from_branch=$from_branch -f to_branch=$to_branch
 
+    Wait-Workflow-Execution $workflow_name
+}
+
+function Wait-Workflow-Execution ($workflow_name) {
     Do
     {
-        Start-Sleep -Seconds 10
+        Start-Sleep -Seconds 30
         $cmdOutput = gh workflow view $workflow_name | Out-String
         $firstLine = ($cmdOutput -split '\n')[5]
         $status = ($firstLine -split '	')[0]

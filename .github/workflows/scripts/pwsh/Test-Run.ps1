53d0b485cd8a63f55cb4eebb84eb1c30cd5633e4
matolera <matiaslera@gmail.com>
2022-04-12 09:43:22 -0300

    update script

--- .github/workflows/scripts/pwsh/Test-Run.ps1
+++ .github/workflows/scripts/pwsh/Test-Run.ps1
@@ -1,5 +1,6 @@
-function Run-Workflow ($githubRef, $solutionName, $environment_url, $source_branch, $branch_to_create, $commit_message) {
-    gh workflow run export-unpack-commit-solution --ref $githubRef -f solution_name=$solutionName -f environment_url=$environment_url -f source_branch=$source_branch -f branch_to_create=$branch_to_create -f commit_message=$commit_message
+function Run-Workflow ($githubRef, $solution_name, $environment_url, $source_branch, $branch_to_create, $commit_message) {
+    gh workflow run export-unpack-commit-solution --ref $githubRef -f solution_name=$solution_name -f environment_url=$environment_url -f source_branch=$source_branch -f branch_to_create=$branch_to_create -f commit_message=$commit_message
 
-    echo "pipeline queued for $solutionName"
+    echo "pipeline queued for $solution_name"
+    echo "pipeline queued for $environment_url"
 }

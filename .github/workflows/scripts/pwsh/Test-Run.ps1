dfe8f7c4b847b41a2e3c0cd75da70f3f5db2aa63
matolera <matiaslera@gmail.com>
2022-04-12 15:52:19 -0300

    update

--- .github/workflows/scripts/pwsh/Test-Run.ps1
+++ .github/workflows/scripts/pwsh/Test-Run.ps1
@@ -1,18 +1,24 @@
-function Run-Workflow ($githubRef, $solution_name, $environment_url, $source_branch, $branch_to_create, $commit_message) {
-    echo "init Run-Workflow"
-    gh workflow run export-unpack-commit-solution --ref $githubRef -f solution_name=$solution_name -f environment_url=$environment_url -f source_branch=$source_branch -f branch_to_create=$branch_to_create -f commit_message=$commit_message
-
-    $status = ""
+function Export-Unpack-Commit ($githubRef, $workflow_name, $solution_name, $environment_url, $source_branch, $branch_to_create, $commit_message) {
+    gh workflow run $workflow_name --ref $githubRef -f solution_name=$solution_name -f environment_url=$environment_url -f source_branch=$source_branch -f branch_to_create=$branch_to_create -f commit_message=$commit_message
 
     Do
     {
-        echo "sleep for 5 seconds"
-        Start-Sleep -Seconds 5
-        $cmdOutput = gh workflow view export-unpack-commit-solution | Out-String
+        Start-Sleep -Seconds 10
+        $cmdOutput = gh workflow view $workflow_name | Out-String
         $firstLine = ($cmdOutput -split '\n')[5]
         $status = ($firstLine -split '	')[0]
-        echo "workflow status: $status" 
-    } While ($status -ne "completed") 
-    
-    echo "end Run-Workflow"
+        echo "$workflow_name status: $status" 
+    } While ($status -ne "completed")
+}
+function Sync-Unmanaged ($githubRef, $workflow_name, $from_branch, $to_branch) {
+    gh workflow run $workflow_name --ref $githubRef -f from_branch=$from_branch -f to_branch=$to_branch
+
+    Do
+    {
+        Start-Sleep -Seconds 10
+        $cmdOutput = gh workflow view $workflow_name | Out-String
+        $firstLine = ($cmdOutput -split '\n')[5]
+        $status = ($firstLine -split '	')[0]
+        echo "$workflow_name status: $status" 
+    } While ($status -ne "completed")
 }

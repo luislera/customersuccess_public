a6a6c4bedf647b021b77882726838142566bb601
Federico Bernal <64086728+FedericoBernal@users.noreply.github.com>
2022-04-13 16:49:55 -0300

    Update Test-Run-Deploy.ps1

--- .github/workflows/scripts/pwsh/Test-Run-Deploy.ps1
+++ .github/workflows/scripts/pwsh/Test-Run-Deploy.ps1
@@ -1,6 +1,6 @@
-function Run-Workflow ($githubRef) {
+function Run-Workflow ($githubRef, $environment, $tag) {
     echo "init Run-Workflow"
-    gh workflow run deploy-tagged-solution-to-environment --ref $githubRef
+    gh workflow run deploy-tagged-solution-to-environment --ref $githubRef -f $environment -f $tag
     echo "sleep for 5 seconds"
     Start-Sleep -Seconds 5
     $cmdOutput = gh workflow view deploy-tagged-solution-to-environment | Out-String

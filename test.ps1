f6ed345e9376a22053bea39d9b9174c122a315b6
Matias Lera <62261539+luislera@users.noreply.github.com>
2022-09-21 10:35:04 -0300

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,4 +1,12 @@
-$Response = Invoke-WebRequest -URI https://www.bing.com?q=how+many+feet+in+a+mile
-$Response.AllElements | Where-Object {
-    $_.name -like "* Value" -and $_.tagName -eq "INPUT"
-} | Select-Object Name, Value
+Write-Host "Loop started"
+
+do
+{
+    $result = Invoke-WebRequest -URI "https://prod-191.westus.logic.azure.com/workflows/f54f8dcc8765421797a099032d809809/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=vJRtV5zX5O6-vfd7IgzPPJGfpSFXvWFbFb8QcCNWVwM"
+    $json = $result | ConvertFrom-Json
+    Write-Host "API status:" $json[0].status
+    Start-Sleep -Seconds 10
+}
+while (($json[0].status -ne 'success'))
+
+Write-Host "Loop ended"

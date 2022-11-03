233c211267aaab4b72f580612e93c3a0e72233c2
Matias Lera <62261539+luislera@users.noreply.github.com>
2022-11-03 13:28:09 -0300

    Update http_requests.ps1

--- http_requests.ps1
+++ http_requests.ps1
@@ -1,22 +1,21 @@
-function TokenGen {
+function GetToken {
     ##########################################################
     # Access Token Request
     ##########################################################
 
     # OAuth Body Access Token Request
-    $authBody = 
+    $authBody =
     @{
         client_id = $env:appId;
-        client_secret = $env:clientSecret;    
-        # The v2 endpoint for OAuth uses scope instead of resource
-        scope = "$($env:dataverseEnvUrl)/.default"    
+        client_secret = $env:clientSecret;
+        scope = "$($env:dataverseEnvUrl)/.default"
         grant_type = 'client_credentials'
     }
 
     $oAuthTokenEndpoint = "https://login.microsoftonline.com/$($env:tenantId)/oauth2/v2.0/token"
 
     # Parameters for OAuth Access Token Request
-    $authParams = 
+    $authParams =
     @{
         URI = $oAuthTokenEndpoint
         Method = 'POST'
@@ -28,27 +27,61 @@ function TokenGen {
     $authRequest = Invoke-RestMethod @authParams -ErrorAction Stop
     return $authRequest
 }
+
+
+function GetBotId{
+    ##########################################################
+    # Get the botId using the bot name
+    ##########################################################
+    param ([string] $botName)
+
+    $apiCallParams =
+    @{
+        URI = "$($env:dataverseEnvUrl)/api/data/v9.2/bots"
+        Headers = @{
+            "Authorization" = "$($authResponse.token_type) $($authResponse.access_token)"
+            "Content-Type" = "application/json"
+        }
+        Method = 'GET'
+    }
+
+    $apiCallRequest = Invoke-RestMethod @apiCallParams -ErrorAction Stop
+    $botId = 0;
+
+    foreach ($bot in $apiCallRequest.value)
+    {
+        if ($botName -eq $bot.name)
+        {
+            $botId = $bot.botid
+            break
+        }
+    }
+
+    return $botId
+}
+
 function PvaPublish{
     ##########################################################
     # First HTTP request, a POST to bots($botId)/PvaPublish
     ##########################################################
+    param ([string] $botId)
 
-    $uriParams = "bots($($env:botId))/Microsoft.Dynamics.CRM.PvaPublish"
+    $uriParams = "bots($(botId))/Microsoft.Dynamics.CRM.PvaPublish"
 
     $apiCallParams =
     @{
         URI = "$($env:dataverseEnvUrl)/api/data/v9.2/$($uriParams)"
         Headers = @{
-            "Authorization" = "$($authResponse.token_type) $($authResponse.access_token)" 
+            "Authorization" = "$($authResponse.token_type) $($authResponse.access_token)"
             "Content-Type" = "application/json"
         }
         Method = 'POST'
     }
 
     $apiCallRequest = Invoke-RestMethod @apiCallParams -ErrorAction Stop
-    $apiCallResponse = $apiCallRequest
-    return $apiCallResponse
+    return $apiCallRequest
 }
+
 function PvaPublishStatus{
     ##########################################################
     # Second HTTP request, a POST to bots($botId)/PvaPublishStatus, using the jobId obtained from previous request.
@@ -66,14 +99,15 @@ function PvaPublishStatus{
         #Body = @{"PublishBotJob" = "$($jobId)"}
         Method = 'POST'
     }
-        $Body = @{"PublishBotJob" = "$($jobId)"}
-        $apiCallRequest = Invoke-RestMethod @apiCallParams -Body ($Body | ConvertTo-Json) -ErrorAction Stop
-        $apiCallResponse = $apiCallRequest
-        Write-Host($apiCallResponse.state)
-        return $apiCallResponse
+
+    $Body = @{"PublishBotJob" = "$($jobId)"}
+    $apiCallRequest = Invoke-RestMethod @apiCallParams -Body ($Body | ConvertTo-Json) -ErrorAction Stop
+    Write-Host($apiCallRequest.state)
+    return $apiCallRequest
 }
-#hash table / dictionary with all the posible states provided
-$PublishingState = 
+
+# Set dictionary with all the posible states provided
+$PublishingState =
 @{
     Unspecified = "Unspecified"
     Submitted = "Submitted"
@@ -87,28 +121,43 @@ $PublishingState =
     NotFound = "NotFound"
     NotAcceptedAlreadyRunning = "NotAcceptedAlreadyRunning"
 }
+
 $Continue = "Submitted","Snapshotting","Validating","Publishing"
-#Token Generation
-$authResponse = TokenGen
-#Call PvaPublish
+
+# Obtain the token
+$authResponse = GetToken
+
+# Get the botId using the bot name variable
+$botId = GetBotId "$($env:botName)"
+
+if ($botId -eq 0)
+{
+    Write-Host("Bot with name $($env:botName) could not be found")
+    Break
+}
+
+# Call PvaPublish
 $PvaPublishResponse = PvaPublish
-#Save the job id into a variable
+
+# Save the job id into a variable
 $jobId= $PvaPublishResponse.PublishBotJobResponse.jobId
-#Save the state into a variable
+
+# Save the initial state into a variable
 $initial_state = $PvaPublishResponse.PublishBotJobResponse.state
 
 Write-Host("Publishing process started! Initial State: $initial_state")
 if($initial_state -ne $PublishingState.Finished){
     $current_state = $initial_state
-    #loop while the Publishing State is not Finished
+    # Loop while the Publishing State is not Finished
     while($Continue -contains $current_state){
-        #Start-Sleep -Seconds 5
+        Start-Sleep -Seconds 5
         switch($current_state) {
             { $PublishingState.Submitted, $PublishingState.Publishing, $PublishingState.Snapshotting, $PublishingState.Validating } {
                 $response = PvaPublishStatus $jobId
-                $current_state = $response.state ; break
+                $current_state = $response.state
+                break
             }
-            ($PublishingState.Finished) {break} 
+            ($PublishingState.Finished) {break}
             Default {break}
         }
     }
